e141331f1e3692f260b5751905f6bf82a7c22df5
Dan DeMass <demassd@gmail.com>
2018-10-22 15:20:55 -0600

    repack post

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+    def index
+      render json: Post.all
+    end
+  
+    def show
+    end
+  
+    def new
+    end
+  
+    def edit
+    end
+  
+    def delete
+      Post.find(params[:id]).destroy
+      render json: { message: 'Post deleted' }
+    end
+  
+  
+    private
+  
+    def post_params
+      params.require(:post).permit(:first_name, :last_name, :body)
+    end
+  end
